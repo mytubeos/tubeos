c4f800c59053af360b7fd62b81d597433a213fd6
Tube Os <tubeos.saas@gmail.com>
2026-04-09 03:19:58 +0530

    Delete frontend/.eslintrc.cjs

--- frontend/.eslintrc.cjs
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  env: { browser: true, es2021: true },
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module', ecmaFeatures: { jsx: true } },
-  rules: { 'no-undef': 'error', 'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }] },
-};
